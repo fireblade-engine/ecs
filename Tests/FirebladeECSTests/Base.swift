22d65c9a6fc7b880b47e316b11c15ec65bf9ce7c
Christian Treffs <christian.treffs@7p-group.com>
2017-10-21 23:34:04 +0200

    Cleanups finished for now

--- Tests/FirebladeECSTests/Base.swift
+++ Tests/FirebladeECSTests/Base.swift
@@ -35,35 +35,3 @@ class Velocity: Component {
 class Party: Component {
 
 }
-
-class DebugEventHandler: EventHandler {
-
-	let eventHub = DefaultEventHub()
-
-	var delegate: EventHub?
-
-	init() {
-		delegate = eventHub
-		subscribe(event: handleEntityCreated)
-		subscribe(event: handleEntityDestroyed)
-		subscribe(event: handleComponentAdded)
-	}
-	deinit {
-		unsubscribe(event: handleEntityCreated)
-		unsubscribe(event: handleComponentAdded)
-		unsubscribe(event: handleEntityDestroyed)
-	}
-
-	func handleEntityCreated(ec: EntityCreated) {
-		print(ec)
-	}
-
-	func handleEntityDestroyed(ed: EntityDestroyed) {
-		print(ed)
-	}
-
-	func handleComponentAdded(ca: ComponentAdded) {
-		print(ca)
-	}
-
-}

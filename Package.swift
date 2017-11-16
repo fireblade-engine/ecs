aa632911ed969e68ac357a54ea257e7b83ec605e
Christian Treffs <christian.treffs@7p-group.com>
2017-11-16 21:55:38 +0100

    Removed unused dependencies

--- Package.swift
+++ Package.swift
@@ -11,11 +11,6 @@ let package = Package(
             name: "FirebladeECS",
             targets: ["FirebladeECS"])
     ],
-    dependencies: [
-        // Dependencies declare other packages that this package depends on.
-        // .package(url: /* package url */, from: "1.0.0"),
-		//.package(url: "https://github.com/PureSwift/CSDL2.git", .branch("master"))
-    ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.

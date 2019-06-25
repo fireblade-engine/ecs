181d8cac519be4c8841d3f37a1cfc47130ce6ee9
Christian Treffs <ctreffs@gmail.com>
2019-06-25 08:10:19 +0200

    Add access control for components

---
+++ Sources/FirebladeECS/Component+Access.swift
@@ -0,0 +1,33 @@
+//
+//  Component+Access.swift
+//  
+//
+//  Created by Christian Treffs on 25.06.19.
+//
+
+@dynamicMemberLookup
+public struct ReadableOnly<Comp> where Comp: Component {
+    @usableFromInline let component: Comp
+    
+    public init(_ component: Comp) {
+        self.component = component
+    }
+    
+    @inlinable public subscript<C>(dynamicMember keyPath: KeyPath<Comp, C>) -> C {
+        return component[keyPath: keyPath]
+    }
+}
+
+@dynamicMemberLookup
+public struct Writable<Comp> where Comp: Component {
+    @usableFromInline let component: Comp
+    
+    public init(_ component: Comp) {
+        self.component = component
+    }
+    
+    @inlinable public subscript<C>(dynamicMember keyPath: ReferenceWritableKeyPath<Comp, C>) -> C {
+        nonmutating get { return component[keyPath: keyPath] }
+        nonmutating set { component[keyPath: keyPath] = newValue }
+    }
+}

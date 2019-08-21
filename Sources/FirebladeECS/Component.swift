81b254480b12f4506a26fdb1e171e1e4dbabb25a
Christian Treffs <ctreffs@gmail.com>
2019-08-21 14:12:22 +0200

    Add component documentation

--- Sources/FirebladeECS/Component.swift
+++ Sources/FirebladeECS/Component.swift
@@ -5,6 +5,10 @@
 //  Created by Christian Treffs on 08.10.17.
 //
 
+/// **Component**
+///
+/// A component represents the raw data for one aspect of the object,
+/// and how it interacts with the world.
 public protocol Component: class {
     static var identifier: ComponentIdentifier { get }
     var identifier: ComponentIdentifier { get }

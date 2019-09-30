2657faff34afecfb68fd625530b9bf034e7370d3
Christian Treffs <ctreffs@gmail.com>
2019-09-30 21:01:19 +0200

    Cleanups

--- Sources/FirebladeECS/EntityIdentifier.swift
+++ Sources/FirebladeECS/EntityIdentifier.swift
@@ -11,19 +11,14 @@ public struct EntityIdentifier {
     /// provides 4294967295 unique identifiers since it's constrained to UInt32 - invalid.
     public let index: Int
 
-    private init() {
-        self = .invalid
-    }
-
     public init(_ uint32: UInt32) {
         self.index = Int(uint32)
     }
 }
 
 extension EntityIdentifier: Equatable { }
-
 extension EntityIdentifier: Hashable { }
-
+extension EntityIdentifier: Codable { }
 extension EntityIdentifier: Comparable {
     @inlinable
     public static func < (lhs: EntityIdentifier, rhs: EntityIdentifier) -> Bool {

d4b8ffbf0c526650063e86015feff264eb19a48b
Christian Treffs <ctreffs@gmail.com>
2019-09-30 21:08:11 +0200

    Add entity identifier comparison test

--- Tests/FirebladeECSTests/EntityTests.swift
+++ Tests/FirebladeECSTests/EntityTests.swift
@@ -21,4 +21,9 @@ class EntityTests: XCTestCase {
         XCTAssertEqual(max, EntityIdentifier.invalid)
         XCTAssertEqual(max.index, Int(UInt32.max))
     }
+    
+    func testEntityIdentifierComparison() {
+        XCTAssertTrue(EntityIdentifier(1) < EntityIdentifier(2))
+        XCTAssertTrue(EntityIdentifier(23) > EntityIdentifier(4))
+    }
 }

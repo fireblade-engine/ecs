1ad4d883f5219b9d37b2259d3391258e6f0dca29
Christian Treffs <ctreffs@gmail.com>
2020-07-09 17:15:06 +0200

    Type entity id

--- Sources/FirebladeECS/EntityIdentifier.swift
+++ Sources/FirebladeECS/EntityIdentifier.swift
@@ -8,12 +8,14 @@
 public struct EntityIdentifier {
     static let invalid = EntityIdentifier(.max)
 
+    public typealias Id = Int
+
     /// provides 4294967295 unique identifiers since it's constrained to UInt32 - invalid.
-    @usableFromInline let id: Int
+    @usableFromInline let id: Id
 
     @usableFromInline
     init(_ uint32: UInt32) {
-        self.id = Int(uint32)
+        self.id = Id(uint32)
     }
 }
 

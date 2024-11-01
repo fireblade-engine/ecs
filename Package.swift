759f5623169ee7d339d9e8e562d07091e3e63fb7
Christian Treffs <ctreffs@gmail.com>
2024-11-01 07:57:20 +0100

    Update documentation workflow (#79)
    * Remove documentation workflow
    
    * Update docc plugin dependency
    
    * Add documentation workflow
    
    * Update hosting basepath
    
    * Update JamesIves/github-pages-deploy-action@v4.6.8

--- Package.swift
+++ Package.swift
@@ -8,7 +8,7 @@ let package = Package(
                  targets: ["FirebladeECS"])
     ],
     dependencies: [
-        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.0.0")
+        .package(url: "https://github.com/apple/swift-docc-plugin", from: "1.4.3")
     ],
     targets: [
         .target(name: "FirebladeECS",
